53af344693ad250440506127f2cecb0dedd4c298
hlhang <hlhang@outlook.com>
2022-10-19 11:49:13 +0800

    add: mixins used

---
+++ packages/my-personal-mp/src/pages/index/Index.ts
@@ -0,0 +1,16 @@
+import { Options, Vue } from 'vue-class-component'
+import { MyJsonConverter } from '@lasted/shared'
+import BasePageDTO from '../beans/BasePageDTO'
+
+@Options({
+  name: 'Index'
+})
+export default class Index extends Vue {
+  value: string = '测试'
+  basePageDTO = new BasePageDTO()
+
+  created() {
+    this.basePageDTO = MyJsonConverter.getInstance().deserializeObject({ page: 1 }, BasePageDTO)
+    console.log(this.basePageDTO)
+  }
+}
---
+++ packages/my-personal-mp/src/pages/index/indexPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <view class="content">
+    {{ basePageDTO?.page }}
+  </view>
+</template>
+
+<script lang="ts">
+import { mixins, Options, Vue } from 'vue-property-decorator'
+import Index from './Index'
+
+@Options({
+  name: 'IndexPage',
+
+})
+export default class IndexPage extends mixins(Index) {
+
+}
+</script>
+
+<style>
+</style>
