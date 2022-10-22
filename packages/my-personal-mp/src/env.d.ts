24d51599f7ea7d1bc5084634e1770d20ea9cccd7
hlhang <hlhang@outlook.com>
2022-10-22 16:36:41 +0800

    add: activity-user-mp dev-env

--- packages/my-personal-mp/src/env.d.ts
+++ packages/my-personal-mp/src/env.d.ts
@@ -1,7 +1,20 @@
-/// <reference types="vite/client" />
-
 interface ImportMetaEnv {
-  readonly VITE_APP_BASE_URL: string
+  readonly VITE_APP_API_TIMEOUT: string
+  readonly VITE_APP_PROJECT_NAME: string
+  readonly VITE_APP_WECHAT_APPID: string
+  readonly VITE_APP_SEVER_APPID: string
+  readonly VITE_APP_CARMELA_APP_URL: string
+  readonly VITE_APP_USER_CENTER_BASE_API: string
+  readonly VITE_APP_ACTIVITY_BASE_API: string
+  readonly VITE_APP_BLOB_IMAGE_URL: string
+  readonly VITE_APP_REPORT_WEB_URL: string
+  readonly VITE_APP_CERT_WEB_URL: string
+  readonly VITE_APP_SHOP_MINI_APP_APPID: string
+  readonly VITE_APP_SEVER_SECRET: string
+  readonly VITE_APP_VERSION: string
+  readonly VITE_APP_THEME_TYPE: string
+  readonly VITE_APP_CHANNEL_CODE: string
+  readonly VITE_APP_LEARNING_BASE_API: string
 }
 
 interface ImportMeta {
