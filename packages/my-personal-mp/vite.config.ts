a332ce22a9559f1252a67b2fd6ca509624e4f979
hlhang <hlhang@outlook.com>
2022-10-22 19:02:07 +0800

    change: import uni-ui way

--- packages/my-personal-mp/vite.config.ts
+++ packages/my-personal-mp/vite.config.ts
@@ -4,6 +4,8 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [uni()],
+  // @ts-ignore
+  optimizeDeps: ['@dcloudio/uni-ui'],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src')
